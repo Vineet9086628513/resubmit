8b3125fd477a0a6de2122c636c195c2eb4de8cbd
Vineet9086628513 <76102266+Vineet9086628513@users.noreply.github.com>
2023-09-27 08:10:44 +0530

    the task is completed

--- index.js
+++ index.js
@@ -19,14 +19,15 @@ const loadPhone = async (searchText, isShowAll) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
+
   const data = await res.json();
   const phones = data.data;
 
   displayPhones(phones, isShowAll);
 };
+
 loadPhone(searchText);
 const displayPhones = (phones, isShowAll) => {
-  //console.log(phones);
   const phoneContainer = document.getElementById("phone-container");
   phoneContainer.textContent = "";
 
@@ -36,14 +37,12 @@ const displayPhones = (phones, isShowAll) => {
   } else {
     showAll.classList.add("hidden");
   }
-  //display first 10
+
   if (!isShowAll) {
     phones = phones.slice(0, 12);
   }
 
   phones.forEach((phone) => {
-    //console.log(phone);
-    //1 create a div
     const phoneCard = document.createElement("div");
     phoneCard.classList = `card bg-base-100 shadow-xl p-5`;
     phoneCard.innerHTML = `
@@ -61,28 +60,15 @@ const displayPhones = (phones, isShowAll) => {
         `;
     phoneContainer.appendChild(phoneCard);
   });
-  //hide loading spinner
+
   loading(false);
-  // function noItem(isNoItem)
-  // {
-  //     if(isNoItem){
-  //         const noItemContainer=document.getElementById("noItem");
-  //         noItemContainer.classList.remove('hidden');
-  //     }
-  //     else{
-  //         noItemContainer.classList.add('hidden');
-  //     }
-  // }
 };
 
-// show All Button
 function showBtn() {
   searchHandler(true);
 }
-// Show Details
+
 const showDetailsHandler = async (id) => {
-  //console.log(id);
-  // load data
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phone/${id}`
   );
@@ -90,7 +76,6 @@ const showDetailsHandler = async (id) => {
 
   const phone = data.data;
   showPhoneDetails(phone);
-  //console.log(phone);
 };
 const showPhoneDetails = (details) => {
   my_modal.showModal();
@@ -104,14 +89,10 @@ const showPhoneDetails = (details) => {
   modelName.innerText = details.name;
   brandName.innerText = `Brand: ${details.brand}`;
   const features = details.mainFeatures;
-  //console.log(features.storage);
+
   console.log(details.image);
   let string = "";
   for (const key in features) {
-    //detailsSpec.innerHTML=`${features[key]} <br>`;
-
-    //detailsSpec.innerText=`${features[key]} <br>`;
-    //console.log(`${key}:${features[key]}`);
     string = string + `${key}: ${features[key]} \n`;
   }
   detailsSpec.innerText = string;
